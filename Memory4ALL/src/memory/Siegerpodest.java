da92b63d6f2a5552cdd2369ff84b6f639df1bf56
LennartanDeMeulen <Lennart.andemei>
2017-06-12 11:41:55 +0200

    Weitere Bearbeitung

--- Memory4ALL/src/memory/Siegerpodest.java
+++ Memory4ALL/src/memory/Siegerpodest.java
@@ -3,28 +3,34 @@ package memory;
 import java.awt.*;
 import javax.swing.*;
 
-public class Siegerpodest {
+public class Siegerpodest extends JFrame {
+	
+	private JFrame podest;
+	private JMenu m1, m2;
+	private JMenuBar m;
+	private JPanel zentrale, seitelinks, seiterechts, buttons;
+	private JButton rgame, ngame, close;
 	
 	
-	public static void main(String[] args) {
+	public Siegerpodest (){
+		
+		super ("Siegerpodest");
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+		setLayout(new BorderLayout());
 
-		JFrame podest = new JFrame("Siegerpodest");
-		podest.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-		podest.setLayout(new BorderLayout());
-
-		// Menüelemente für den oberen Rand
-		JMenu m1 = new JMenu("Anleitung");
+		// Menueelemente fuer den oberen Rand
+		m1 = new JMenu("Anleitung");
 		m1.add("Ansehen");
 		m1.add("Punkte ergänzen");
 		m1.setBackground(Color.lightGray);
 
-		JMenu m2 = new JMenu("Impressum");
+		m2 = new JMenu("Impressum");
 		m2.add("Ansehen");
 		m2.add("Kontakt aufnehmen");
 		m2.setBackground(Color.lightGray);
 
-		// Zusammenfassung der Menüleiste
-		JMenuBar m = new JMenuBar();
+		// Zusammenfassung der Menueleiste
+		m = new JMenuBar();
 		m.add(m1);
 		m.add(m2);
 		m.setBackground(Color.lightGray);
@@ -54,47 +60,53 @@ public class Siegerpodest {
 		ImageIcon podestpic = new ImageIcon(podestpicresize);
 		JLabel podestbild = new JLabel(podestpic);
 
-		JPanel zentrale = new JPanel(new GridLayout(1, 1));
+		zentrale = new JPanel(new GridLayout(1, 1));
 		zentrale.add(podestbild);
 		zentrale.setBackground(Color.cyan);
 
 		// Seitenfarben
-		JPanel seitelinks = new JPanel();
+		seitelinks = new JPanel();
 		seitelinks.setBackground(Color.CYAN);
-		JPanel seiterechts = new JPanel();
+		seiterechts = new JPanel();
 		seiterechts.setBackground(Color.CYAN);
 
 		// Buttons am Ende
-		JButton ngame = new JButton("Spiel neu konfigurieren?");
+		ngame = new JButton("Spiel neu konfigurieren?");
 		ngame.setBackground(Color.YELLOW);
 		ngame.setFont(new Font("MS Gothic", Font.BOLD, 20));
-
-		JButton rgame = new JButton("Spiel erneut beginnen?");
+		
+		rgame = new JButton("Spiel erneut beginnen?");
 		rgame.setBackground(Color.green);
 		rgame.setFont(new Font("MS Gothic", Font.BOLD, 20));
 
-		JButton close = new JButton("Anwendung schließen");
+		close = new JButton("Anwendung schliessen");
 		close.setBackground(Color.red);
 		close.setFont(new Font("MS Gothic", Font.BOLD, 20));
 
 		// Button in ein Panel einpassen
-		JPanel buttons = new JPanel(new GridLayout(1, 3));
+		buttons = new JPanel(new GridLayout(1, 3));
 		buttons.add(close);
 		buttons.add(ngame);
 		buttons.add(rgame);
 
 		// Anordnung im Frame
-		podest.add(m, BorderLayout.NORTH);
-		podest.add(seitelinks, BorderLayout.WEST);
-		podest.add(zentrale, BorderLayout.CENTER);
-		podest.add(seiterechts, BorderLayout.EAST);
-		podest.add(buttons, BorderLayout.SOUTH);
+		add(m, BorderLayout.NORTH);
+		add(seitelinks, BorderLayout.WEST);
+		add(zentrale, BorderLayout.CENTER);
+		add(seiterechts, BorderLayout.EAST);
+		add(buttons, BorderLayout.SOUTH);
 
-		// Weitere Einstellung für das Podest Frame
-		podest.setVisible(true);
-		podest.setBounds(400, 50, 1100, 750);
-		podest.setResizable(true);
+		// Weitere Einstellung fuer das Podest Frame
+		setVisible(true);
+		setBounds(400, 50, 1100, 750);
+		setResizable(true);
 
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		new Siegerpodest ();
 	}
 
 }
